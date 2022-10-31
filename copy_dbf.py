00bbc6e4bf56232cbae3616619f73b38aa768543
Кожин Роман <resume-gl@yandex.ru>
2022-10-31 12:43:35 +0300

    добавил прогресс бар

--- copy_dbf.py
+++ copy_dbf.py
@@ -3,6 +3,8 @@ from typing import List
 import re
 import shutil
 import hashlib
+from progress.bar import Bar, IncrementalBar
+
 """
 скрипт раскопирования файлов dbf
 из папки 
@@ -24,7 +26,7 @@ def get_hash_md5(filename):
         return m.hexdigest()
 
 
-def read_file_in_folder(top_folder: str = '', folder_shops: List = [], folder_destination: str = '') -> None:
+def read_file_in_folder(top_folder: str = '', folder_shops: List = [], folder_destination: list = []) -> None:
     """
     проходим по папке магазина смотрим дату создания файлов dbf
     и если в конечной папке нет этого файла или у него дата другая
@@ -38,29 +40,35 @@ def read_file_in_folder(top_folder: str = '', folder_shops: List = [], folder_de
     sub_dir_dest = 'dbf'  #конечная папка для аналитикии бекапов
 
     pattern = r'[A-Za-z]{2}[A-Ca-c0-9]{5}[N,Z]{1}.dbf'  #шаблон должен покрывать файлы такого вида KB10922Z.dbf
+    shop_bar = Bar('SHOPS', max=len(folder_shops))
     for shop in folder_shops:   #проходим по папкам магазинов
+        shop_bar.next()
         inbox_export = top_folder + shop + '\\' +sub_dir_inbox
         filenames = next(os.walk(inbox_export), (None, None, []))[2]   #собираем список файлов
+        files_bar = IncrementalBar('FILES in {0}'.format(shop), max=len(filenames))
         for files in filenames:
+            files_bar.next()
             if re.fullmatch(pattern, files) is not None:
-                # тут надо сравнить время первого файла и второго
+                # тут надо сравнить хэши первого файла и второго
                 source_file = inbox_export + '\\' + files  #полное имя исходного файла
-                dest_file = folder_destination + '\\' + shop + '\\' + sub_dir_dest + '\\' + files  #полное имя конечного файла
-                copy_yes = False  #флаг делать копирование
-                if os.path.exists(dest_file):  #если конечный айл существует то надо его время посмотреть
-                    hash_source = get_hash_md5(source_file)
-                    dest_source = get_hash_md5(dest_file)
-                    # print(hash_source)
-                    # print(dest_source)
-                    if hash_source != dest_source:
+                hash_source = get_hash_md5(source_file)
+                for elem in folder_destination:
+                    dest_file = elem + '\\' + shop + '\\' + sub_dir_dest + '\\' + files  #полное имя конечного файла
+                    copy_yes = False  #флаг делать копирование
+                    if os.path.exists(dest_file):  #если конечный файл существует то надо сравнить хэши файлов
+                        dest_source = get_hash_md5(dest_file)
+                        # print(hash_source)
+                        # print(dest_source)
+                        if hash_source != dest_source:
+                            copy_yes = True
+                            print('ФАЙЛЫ ОТЛИЧАЮТСЯ! скопирован файл1 {0} в файл2 {1}'.format(source_file, dest_file))
+                    else:
                         copy_yes = True
-                        print('ФАЙЛЫ ОТЛИЧАЮТСЯ! скопирован файл1 {0} в файл2 {1}'.format(source_file, dest_file))
-                else:
-                    copy_yes = True
-                    print('ФАЙЛА НЕТ! скопирован файл {0} в файл {1}'.format(source_file, dest_file))
-                if copy_yes == True:
-                    pass
-                    shutil.copy2(source_file, dest_file)
+                        print('ФАЙЛА НЕТ! скопирован файл {0} в файл {1}'.format(source_file, dest_file))
+                    if copy_yes == True:
+                        shutil.copy2(source_file, dest_file)
+        files_bar.finish()
+    shop_bar.finish()
 
 
 
@@ -84,12 +92,11 @@ def main():
     """
     r_path = r'\\shoprsync\\rsync\\inbox\\'
     folder_shops = read_path(in_path=r_path)   #получаем список папок магазнов
-    folder_shops = ['TT']
     write_path = [r'\\shoprsync\\magazin\\', r'\\store\\junk\\90_Файлсервер\\backup\\dbf\\']  #пути куда нам надо закинуть наши dbf
-    # write_path = [r'\\shoprsync\\magazin\\']  # пути куда нам надо закинуть наши dbf
-    for elem in write_path:
-        read_file_in_folder(top_folder=r_path, folder_shops=folder_shops, folder_destination=elem)
-
+    write_path = [r'\\shoprsync\\magazin\\']  # пути куда нам надо закинуть наши dbf
+    # for elem in write_path:
+    #     read_file_in_folder(top_folder=r_path, folder_shops=folder_shops, folder_destination=elem)
+    read_file_in_folder(top_folder=r_path, folder_shops=folder_shops, folder_destination=write_path)
 
 if __name__ == '__main__':
     main()
